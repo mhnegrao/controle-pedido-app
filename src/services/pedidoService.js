09c3a53a97c3e08b0eebf063ca45b4c165c4c3b7
Marcelo Henrique <mhnegrao@gmail.com>
2021-04-04 20:02:24 -0300

    correção de formatação de arquivo

--- src/services/pedidoService.js
+++ src/services/pedidoService.js
@@ -1,5 +1,4 @@
 const pedidosData = require('../data/pedidoData');
-
 exports.show = (req,res) => pedidosData.show(req,res);
 exports.create = (pedidos) => pedidosData.create(pedidos);
 exports.find = () => pedidosData.find();
